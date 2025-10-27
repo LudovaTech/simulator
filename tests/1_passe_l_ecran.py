3008f06f13001cf4f5f188cf05d0d02116fcfab0
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2025-10-27 22:41:13 +0100

    meilleurs tutoriels

---
+++ tests/1_passe_l_ecran.py
@@ -0,0 +1,6 @@
+# nom de l'équipe, sert d'identifiant
+TEAM_NAME = "passe"
+
+# fonction appelée à chaque tour pour savoir l'action que veut faire un robot
+def update(data):
+    return None
---
+++ tests/3_poursuit_balle.py
@@ -0,0 +1,16 @@
+from random import randint
+
+TEAM_NAME = "vers balle"
+
+# data contient :
+# my_position: (float, float)
+# friend_position: (float, float)
+# enemy1_position: (float, float)
+# enemy2_position: (float, float)
+# ball_position: (float, float)
+def update(data):
+    return {
+        "target_position": data["ball_position"],
+        "power": randint(150, 255), # pour que les robots ne soient pas bloqués au centre
+        "target_orientation": 90
+    }
